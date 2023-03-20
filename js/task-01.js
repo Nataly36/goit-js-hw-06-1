f02a6a3d95479a41bc78812a948a83cbda6f679f
Nataly36 <movienata25@gmail.com>
2023-03-20 19:39:09 +0200

    new1

--- js/task-01.js
+++ js/task-01.js
@@ -1,35 +1,20 @@
 const numberOfCategories = document.querySelectorAll(".item");
-console.log('Number of categories:', numberOfCategories.length)
-// console.log('Number of categories:', numberOfCategories)
+console.log('Number of categories:', numberOfCategories.length);
 
 const Category = document.querySelector(" h2");
-console.log('Category: ', Category.textContent)
+console.log('Category: ', Category.textContent);
+const elementsCategFist = document.querySelector('.item ul');
 
-const kjhgf = document.querySelector('.item ul')
-// console.log(kjhgf)
-const rtgn = kjhgf.children
-console.log('Elements:', rtgn.length)
+// console.log(elementsCategFist)
+const ElementsFist = elementsCategFist.children;
+console.log('Elements:', ElementsFist.length);
 
-const hyt = document.querySelector(" li");
-// console.log(hyt)
+const elementsCategSecond = document.querySelectorAll("#categories h2")[1];
+console.log('Category: ', elementsCategSecond.textContent)
+const elementSecond = elementsCategSecond.nextElementSibling.children;
+console.log('Elements:',elementSecond.length)
 
-const ftyu = hyt.nextSibling
-
-// console.log(ftyu)
-const nnmv=ftyu.nextElementSibling
-// console.log(nnmv)
-const wre = nnmv.querySelector(" h2");
-console.log('Category: ', wre.textContent)
-const asdf = nnmv.querySelector(" ul");
-// console.log(asdf)
-const frdet = asdf.children
-console.log('Elements:', frdet.length)
-
-const fds = nnmv.nextElementSibling
-// console.log(fds)
-const nbvc = fds.querySelector(" h2");
-console.log('Category: ', nbvc.textContent)
-const nmk = fds.querySelector(" ul");
-// console.log(nmk)
-const frt = nmk.children
-console.log('Elements:',frt.length)
+const elementsCategLast = document.querySelectorAll("#categories h2")[2];
+console.log('Category: ', elementsCategLast.textContent);
+const childLastEl = elementsCategLast.nextElementSibling.children;
+console.log('Elements:',childLastEl.length)
