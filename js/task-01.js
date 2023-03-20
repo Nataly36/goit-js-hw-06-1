ef2b7485cdcd9f9f235d93de30f82c12ebe9d6d9
Nataly36 <movienata25@gmail.com>
2023-03-20 20:44:28 +0200

    1.1

--- js/task-01.js
+++ js/task-01.js
@@ -1,21 +1,9 @@
 const numberOfCategories = document.querySelectorAll(".item");
 console.log('Number of categories:', numberOfCategories.length);
+console.log(numberOfCategories)
+numberOfCategories.forEach((item )=> {
+    console.log('Category: ', item.firstElementChild.textContent),
+     console.log('Elements:',item.lastElementChild.children.length);
+})
+    
 
-const Category = document.querySelector(" h2");
-console.log('Category: ', Category.textContent);
-const elementsCategFist = document.querySelector('.item ul');
-
-
-const ElementsFist = elementsCategFist.children;
-console.log('Elements:', ElementsFist.length);
-
-
-const elementsCategSecond = document.querySelectorAll("#categories h2")[1];
-console.log('Category: ', elementsCategSecond.textContent)
-const elementSecond = elementsCategSecond.nextElementSibling.children;
-console.log('Elements:',elementSecond.length)
-
-const elementsCategLast = document.querySelectorAll("#categories h2")[2];
-console.log('Category: ', elementsCategLast.textContent);
-const childLastEl = elementsCategLast.nextElementSibling.children;
-console.log('Elements:',childLastEl.length)
