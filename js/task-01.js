365d410dc06526677169c5e2e402c470967d36a5
Nataly36 <movienata25@gmail.com>
2023-03-20 20:46:40 +0200

    1.2

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,6 @@
 const numberOfCategories = document.querySelectorAll(".item");
 console.log('Number of categories:', numberOfCategories.length);
-console.log(numberOfCategories)
+
 numberOfCategories.forEach((item )=> {
     console.log('Category: ', item.firstElementChild.textContent),
      console.log('Elements:',item.lastElementChild.children.length);
